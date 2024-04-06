05085391c579a42c74aa162237d56646fcecb9a5
KoolTheba <gomez.teba@gmail.com>
2024-04-06 10:50:30 +0200

    test: adapted build fake data to override data and added specific test

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -7,14 +7,12 @@ import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 
-
-const buildLoginForm = () => {
-  const username = faker.internet.userName()
-  const password = faker.internet.password()
-  
+// allow args overrides
+const buildLoginForm = (args) => {
   return {
-    username,
-    password
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    ...args
   }
 }
 
@@ -72,6 +70,29 @@ test('submitting the form calls onSubmit with generated test data', async () => 
   const passwordInput =  screen.getByLabelText(/password/i)
   const submitButton = screen.getByRole('button', {name: /submit/i})
   const {username, password} = buildLoginForm()
+  
+  await userEvent.type(usernameInput, username)
+  await userEvent.type(passwordInput, password)
+  await userEvent.click(submitButton)
+  
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username,
+    password
+  })
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+})
+
+
+test('submitting the form calls onSubmit allowing data overrrides', async () => {
+  const handleSubmit = jest.fn();
+  
+  render(<Login onSubmit={handleSubmit}/>)
+  const usernameInput = screen.getByRole('textbox', {
+    name: /username/i
+  })
+  const passwordInput =  screen.getByLabelText(/password/i)
+  const submitButton = screen.getByRole('button', {name: /submit/i})
+  const {username, password} = buildLoginForm({password: 'abc'})
   
   await userEvent.type(usernameInput, username)
   await userEvent.type(passwordInput, password)
