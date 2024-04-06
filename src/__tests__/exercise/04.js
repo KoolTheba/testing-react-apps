5ac80faa5fbab5e31ee7b6bca570c2eb2bd18214
KoolTheba <gomez.teba@gmail.com>
2024-04-06 10:33:29 +0200

    test: created new test using jest mock function for handleSubmit

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -27,3 +27,25 @@ test('submitting the form calls onSubmit with username and password', async () =
     password
   })
 })
+
+test('submitting the form calls onSubmit with jest mock function', async () => {
+  const handleSubmit = jest.fn();
+  
+  render(<Login onSubmit={handleSubmit}/>)
+  const usernameInput = screen.getByRole('textbox', {
+    name: /username/i
+  })
+  const passwordInput =  screen.getByLabelText(/password/i)
+  const submitButton = screen.getByRole('button', {name: /submit/i})
+  const username = 'myname'
+  const password = 'mypassword'
+  await userEvent.type(usernameInput, username)
+  await userEvent.type(passwordInput, password)
+  await userEvent.click(submitButton)
+  
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username,
+    password
+  })
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+})
